4a7d821b8ce0bae9fd204fb098f66ee81d0e08c5
Devlin Junker <devlin.junker@eduworks.com>
2017-10-01 20:45:18 -0700

    working with linting and documentation, parsed by babel into dist/ directory

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * A function that adds two numbers
+ * @param  {number} a first number
+ * @param  {number} b second number
+ * @return {number}   numbers added together
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(1, 2));
