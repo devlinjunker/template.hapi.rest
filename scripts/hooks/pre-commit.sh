911184bd6aa62d34d0fb266ddb8b7a2b1d65d4b6
devlinjunker <devlin.junker@gmail.com>
2020-03-21 14:26:05 -0700

    move master check to beginning so we don't build in master before cancel

--- scripts/hooks/pre-commit.sh
+++ scripts/hooks/pre-commit.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 # Hook before commit message is requested from user
-#   - Lint/Compile application to make sure we don't commit bad code
 
-# For testing what is set by Husky
-# echo $HUSKY_GIT_PARAMS;
 CURRENT_BRANCH=`git branch | grep \* | cut -d' ' -f2`;
 
+# This should prevent us from making any commits to master branch
+if [[ "$CURRENT_BRANCH" == "master" ]]; then
+  echo "\n\nNo commiting to the master branch!\n\n";
+  exit 123545;
+fi
+
 ## All Branches
+#   - Lint/Compile application to make sure we don't commit bad code
 
 # stash any unsaved changes and then pop after running all the tests. That way uncommited changes won't affect tests
 git stash save -k "githook uncommitted changes";
@@ -18,8 +22,3 @@ npm run build;
 
 git stash pop;
 
-# This should prevent us from making any commits to master branch
-if [[ "$CURRENT_BRANCH" == "master" ]]; then
-  echo "No commiting to the master branch"
-  exit 123545;
-fi
