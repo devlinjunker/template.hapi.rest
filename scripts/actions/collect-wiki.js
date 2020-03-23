2feeccaf0552600c06dcf075cc96a151cd03dc6f
devlinjunker <devlin.junker@gmail.com>
2020-03-23 08:32:23 -0700

    start script to collect readmes and rename files

---
+++ scripts/actions/collect-wiki.js
@@ -0,0 +1,22 @@
+
+const path = require('path');
+const fs = require('fs');
+const esdocSettings = require('../../.esdoc.json');
+
+const TMP_DIR = 'wiki';
+
+
+const tmpPath = path.join(__dirname, '../..', TMP_DIR);
+if (!fs.existsSync(tmpPath)) {
+  fs.mkdirSync(tmpPath);
+}
+
+const readmeFiles = esdocSettings['plugins'][0]['option']['manual']['files'];
+for (const i in readmeFiles) {
+  const fullPath = '../.' + readmeFiles[i];
+  const name = path.parse(fullPath).base;
+
+
+  const newName = i + '-' + name.substr(7, name.length - 3);
+  fs.copyFileSync(path.resolve(__dirname, fullPath), path.join(tmpPath, newName));
+}
