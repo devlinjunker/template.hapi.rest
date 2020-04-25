e73229d4651e4141a089f0e37e04ae19847259f1
devlinjunker <devlin.junker@gmail.com>
2020-04-25 14:33:50 -0700

    fixes to wiki sync script so links in home page work on github

--- scripts/actions/collect-wiki.js
+++ scripts/actions/collect-wiki.js
@@ -11,17 +11,33 @@ if (!fs.existsSync(tmpPath)) {
   fs.mkdirSync(tmpPath);
 }
 
+const map = {};
 const readmeFiles = esdocSettings['plugins'][0]['option']['manual']['files'];
 for (const i in readmeFiles) {
   const fullPath = `../.${  readmeFiles[i]}`;
   const name = path.parse(fullPath).base;
 
-
   const newName = `${i  }-${  name.substr(7, name.length - 3)}`;
+
+  map[name] = newName;
   fs.copyFileSync(path.resolve(__dirname, fullPath), path.join(tmpPath, newName));
 }
 
 // Attempt copy index file to "home.md" in wiki
 const home = esdocSettings['plugins'][0]['option']['manual']['index'];
 const homePath = `../.${home}`;
-fs.copyFileSync(path.resolve(__dirname, homePath), path.join(tmpPath, 'home.md'));
+let content = fs.readFileSync(path.resolve(__dirname, homePath)).toString();
+
+for (const oldName in map) {
+  // oldName: "README.controllers.md";
+  // become: newName
+  //
+  // find (manual/README.controllers.html)
+  const newName = map[oldName];
+  const namePart = oldName.substring(0, oldName.length - 3);
+
+  const regexp = new RegExp(`\\(manual\\/${namePart}\\.html\\)`);
+  content = content.replace(regexp, `(${newName})`);
+}
+
+fs.writeFileSync(path.join(tmpPath, 'Home.md'), content);
